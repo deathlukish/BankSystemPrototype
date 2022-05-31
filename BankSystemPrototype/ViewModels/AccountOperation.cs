0bda333edbd56ccbc33483767e8ec75d86d0fadf
Дмитрий <deathlukish@mail.ru>
2022-05-31 16:03:16 +0300

    открытие депозитного счета

--- BankSystemPrototype/ViewModels/AccountOperation.cs
+++ BankSystemPrototype/ViewModels/AccountOperation.cs
@@ -1,13 +1,16 @@
 ﻿using BankSystemPrototype;
 using BankSystemPrototype.Commands;
 using BankSystemPrototype.ViewModels;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace BankClientOperation
 {
     internal class AccountOperation<T> : ViewModel
-        where T : BaseClient
+        where T : BaseClient, new()
     {
         Repository _Repository = new Repository();
         private ObservableCollection<T> _Clients = new();
@@ -15,30 +18,39 @@ namespace BankClientOperation
         private ObservableCollection<BaseAccount> _AccountsTo = new();
         private BaseAccount _SelectedAccountFrom;
         private BaseAccount _SelectedAccountTo;
-        private T _SelectedClientFrom;
+        private T _SelectedClientFrom = new(); 
         private T _SelectedClientTo;
         private float _ReplenishSum;
         private T Client;
         private string _FirstName;
         public ICommand AddClientCommand { get; }
-        public ICommand SaveChange { get; }
+        public ICommand OpenDeposite { get; }
         private void OnAddClient(object p)
         {
             
             var _AddClient = new AddClient();
            _AddClient.Show();
         }
-        private bool CanAddClient(object p) => true;
-        private void OnSaveChange(object p)
+        private void OnOpenDeposite(object p)
         {
-
-            _Repository.SaveBase();
+            
+            SelectedClientFrom.Accounts.Add(new Deposite(SelectedClientFrom.IdClient));
         }
-        private bool CanSaveChange(object p) => true;
+        private bool CanAddClient(object p) => true;
+        private bool CanOpenDeposite(object p)
+        {
+          //  if (SelectedClientFrom == null) return false;
+            if (SelectedClientFrom.Accounts == null) SelectedClientFrom.Accounts = new List<BaseAccount>();
+            if ((SelectedClientFrom.Accounts.FindAll(e => e is Deposite).Count == 0)) return true;
+            else return false;
+        
+        
+        }
         public AccountOperation()
         {
             GetClients();
             AddClientCommand = new RelayCommand(OnAddClient, CanAddClient);
+            OpenDeposite = new RelayCommand(OnOpenDeposite, CanOpenDeposite);
         }
         public BaseAccount SelectedAccountFrom
         {
@@ -67,27 +79,24 @@ namespace BankClientOperation
             }
 
         }
-        //public T SelectedClientFrom
-        //{
-        //    get => _SelectedClientFrom;
-        //    set
-        //    {
-        //        Set(ref _SelectedClientFrom, value);
-        //        AccountsFrom = _Repository.GetAccountsFromClient(SelectedClientFrom.IdClient);
-        //    }
+        public T SelectedClientFrom
+        {
+            get => _SelectedClientFrom;
+            set
+            {
+                Set(ref _SelectedClientFrom, value);
+             
+            }
 
 
-        //}
+        }
         public string FirsName 
         {
             get => _FirstName;
             set => Set(ref _FirstName, value);
         }
 
-        public void Replenish()
-        {
 
-        }
         public void OpenAccount()
         {
         
