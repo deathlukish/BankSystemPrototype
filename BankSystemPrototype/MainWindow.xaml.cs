8bef2686bda793e449f46b65dab56f0ef502712e
Дмитрий <deathlukish@mail.ru>
2022-06-15 10:47:19 +0300

    не будем вые, уберем mvvm

---
+++ BankSystemPrototype/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace BankSystemPrototype
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
